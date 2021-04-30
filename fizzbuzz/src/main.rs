332dfd0d6dec438155e056859df5f847698e4717
andy-thomason <andy@andythomason.com>
2021-04-30 10:01:05 +0100

    Fizzbuzz

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,24 @@
+
+fn fizzbuzz1(i: u32) -> String {
+    match (i % 3, i % 5) {
+        (0, 0) => "FizzBuzz".to_owned(),
+        (0, _) => "Fizz".to_owned(),
+        (_, 0) => "Buzz".to_owned(),
+        (_, _) => format!("{}", i),
+    }
+}
+
+fn fizzbuzz2(i: u32) -> String {
+    match i % 15 {
+        0 => "FizzBuzz".to_owned(),
+        3 | 6 | 9 | 12 => "Fizz".to_owned(),
+        5 | 10 => "Buzz".to_owned(),
+        _ => format!("{}", i),
+    }
+}
+
+fn main() {
+    for i in 0..31 {
+        println!("{:10} {:10}", fizzbuzz1(i), fizzbuzz2(i));
+    }
+}
